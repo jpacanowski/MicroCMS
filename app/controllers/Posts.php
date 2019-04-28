db96abf5e84a578b3146961797975ce8e9fad6d5
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2019-04-28 10:16:49 +0200

    Add site_url in Post views

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -5,34 +5,41 @@ class Posts extends Controller {
     public function __construct() {
         $this->postModel = $this->model('Post');
         $this->menuModel = $this->model('Navigation');
+        $this->settingModel = $this->model('Setting');
     }
 
     public function index() {
         $posts = $this->postModel->getPosts();
         $menu = $this->menuModel->getNavigation();
+        $settings = $this->settingModel->getSettings();
         $this->view('posts/index', [
             'posts' => $posts,
-            'menu' => $menu
+            'menu' => $menu,
+            'cms' => $settings
         ]);
     }
 
     public function show($slug) {
         $post = $this->postModel->getPost($slug);
         $comments = $this->postModel->getPostComments($post->id);
+        $settings = $this->settingModel->getSettings();
         $menu = $this->menuModel->getNavigation();
         $this->view('posts/single', [
             'post' => $post,
             'comments' => $comments,
+            'cms' => $settings,
             'menu' => $menu
         ]);
     }
 
     public function tag($tag) {
         $posts = $this->postModel->getPostsByTag($tag);
+        $settings = $this->settingModel->getSettings();
         $menu = $this->menuModel->getNavigation();
         $this->view('posts/index', [
             'posts' => $posts,
-            'menu' => $menu
+            'menu' => $menu,
+            'cms' => $settings
         ]);
     }
 
