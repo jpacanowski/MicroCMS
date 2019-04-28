88465ec4082ed379439026394182dfa2e0168805
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2019-04-28 10:23:06 +0200

    Add site_url in Page views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,14 +5,17 @@ class Pages extends Controller {
     public function __construct() {
         $this->pageModel = $this->model('Page');
         $this->menuModel = $this->model('Navigation');
+        $this->settingModel = $this->model('Setting');
     }
 
     public function show($slug) {
         $page = $this->pageModel->getPage($slug);
         $menu = $this->menuModel->getNavigation();
+        $settings = $this->settingModel->getSettings();
         $this->view('pages/single', [
             'page' => $page,
-            'menu' => $menu
+            'menu' => $menu,
+            'cms' => $settings
         ]);
     }
 }
